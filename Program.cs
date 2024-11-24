546ed69b6cb9e9291a774106213bed9883d336b5
Enyo <senyobrah@gmail.com>
2024-11-25 01:06:23 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace StringReverse;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a string to reverse: ");
+        string input = Console.ReadLine();
+
+        string reversed = ReverseString(input);
+        
+        Console.WriteLine(reversed);
+    }
+
+    static string ReverseString(string str)
+    {
+        char[] charArray = str.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
